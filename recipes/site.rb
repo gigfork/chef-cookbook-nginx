31c57a597ee485e9a825412c52654fd8fdabc596
James Kyle <james@jameskyle.org>
2012-08-24 16:33:24 -0700

    Doh. Easing back into the dsl, fixed syntax error in template dsl.

--- recipes/site.rb
+++ recipes/site.rb
@@ -5,11 +5,6 @@ include_recipe "nginx::default"
 
 template "site.conf" do
   path          "#{node['nginx']['dir']}/conf.d/<%= node['nginx']['site']['sitename'] %>.conf" 
-  port_based    node['nginx']['site']['port_based']
-  server_ports  node['nginx']['site']['server_ports']
-  sitename      node['nginx']['site']['sitename']
-  ssl_cert      node['nginx']['site']['ssl_cert']
-  ssl_key       node['nginx']['site']['ssl_key']
   mode          0644
   owner         "root"
   group         "root"
