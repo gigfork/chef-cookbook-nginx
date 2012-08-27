fb9b920bfae39d76115c846d2ea42f5eaafa478e
James Kyle <james@jameskyle.org>
2012-08-27 10:44:58 -0700

    finalized nginx site deploy w/ snakeoil (or other specified certificate)

--- recipes/site.rb
+++ recipes/site.rb
@@ -3,8 +3,28 @@
 # Recipe:: site
 include_recipe "nginx::default"
 
+if node['nginx']['site']['snakeoil'] then
+  case node[:platform]
+  when "ubuntu", "debian"
+    package "ssl-cert" do
+      action :install
+    end
+  end
+  
+  if not File.exists? "/etc/ssl/certs/ssl-cert-snakeoil.pem" then
+    script "install_something" do
+      interpreter "bash"
+      user "root"
+      code <<-EOH
+      /usr/sbin/make-ssl-cert generate-default-snakeoil
+      EOH
+    end
+  end
+end
+
+
 template "site.conf" do
-  path          "#{node['nginx']['dir']}/conf.d/<%= node['nginx']['site']['sitename'] %>.conf" 
+  path          "#{node['nginx']['dir']}/conf.d/#{node['nginx']['site']['sitename']}.conf" 
   mode          0644
   owner         "root"
   group         "root"
