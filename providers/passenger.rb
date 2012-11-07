dbd4e941aaf1b4078e93170d11f9322e3db30e18
James Kyle <james@jameskyle.org>
2012-11-07 13:52:52 -0800

    Updated to conform with new rubyapp deploy resource based deployment

--- providers/passenger.rb
+++ providers/passenger.rb
@@ -6,11 +6,10 @@ action :create do
     owner "root"
     group "root"
     variables :site_name => new_resource.name,
-              :app_name  => new_resource.app_name,
               :ssl_cert  => new_resource.ssl_cert,
               :ssl_key   => new_resource.ssl_key,
               :rails_env => new_resource.rails_env,
-              :root      => new_resource.root
+              :docroot   => new_resource.docroot
     notifies :reload, resources(:service => "nginx"), :immediately
   end
 end
