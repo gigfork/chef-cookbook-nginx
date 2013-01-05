8d42ea008fb84100c6b18ac660d9be3d9d41a793
James Kyle <james@jameskyle.org>
2013-01-04 17:05:58 -0800

    Updated site resource for the final data bag format for upstreams

--- resources/site.rb
+++ resources/site.rb
@@ -2,4 +2,4 @@ actions :create, :delete
 
 attribute :fqdn,      :kind_of => String, :name_attribute => true
 attribute :servers,   :kind_of => Array,  :default => Array.new
-attribute :upstreams, :kind_of => Array,  :default => Array.new 
+attribute :upstreams, :kind_of => Hash,  :default => Hash.new 
